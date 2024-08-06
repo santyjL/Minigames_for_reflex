be3254c1c65780f1d52b3d8df3f3b0a8c4088ca8
santyjL <jlopezsanty1@gmail.com>
2024-08-06 12:31:02 -0600

    se crearon los primeros estilos

---
+++ MiniGames/styles.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class TamañosTexto(Enum):
+    TITULO = "2.5em"
+    SUBTITULO = "2em"
+    TEXTO = "1.5em"
+
+class Colores(Enum):
+    BG = "linear-gradient(#7A0C0C , #8B89E7)"
+    PRINCIPAL = "#DE1010"
+    SECUNDARIO = "#0A0897"
+
+    TEXTO = "#C1C1C1"
+    SUBTITULO = "#FFFFFF"
+    TITULO = "#000000"
+
+class Tamaños (Enum):
+    MARGIN_PEQUEÑO = "0.5em"
+    MARGIN_MEDIANO = "1em"
+    MARGIN_GRANDE = "2em"
+
+    PADDING = "1em"
+
+    BORDER_RADIUS = "2em"
+
+    BORDER = "solid #000000"
