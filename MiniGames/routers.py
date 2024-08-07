3b44366a795747dfe897a617e5f54af257d62602
santyjL <jlopezsanty1@gmail.com>
2024-08-06 19:50:42 -0600

    se crearon los routers

---
+++ MiniGames/routers.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class routers(Enum):
+    PRINCIPAL =  "/"
+    ENCUENTRA_EL_NUMERO = "/Encuentra-el-numero"
+    TRES_EN_RAYA = "/Tres-en-raya"
+    PIEDRA_PAPEL_TIJERAS = "/Piedra-papel-tijeras-lagarto-spock"
