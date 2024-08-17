428957f69ce57106b8cec31414fc4ebc8deacaff
santyjL <jlopezsanty1@gmail.com>
2024-08-17 14:33:34 -0600

    se añadio la pagina del primer juego encuentra el numero

--- MiniGames/MiniGames.py
+++ MiniGames/MiniGames.py
@@ -2,8 +2,10 @@
 
 import reflex as rx
 
+from MiniGames.page.juego1 import pantalla_juego1
 from MiniGames.page.principal import pantalla_principal
 from MiniGames.routers import routers
 
 app = rx.App()
 app.add_page(pantalla_principal , route=routers.PRINCIPAL.value)
+app.add_page(pantalla_juego1 , route=routers.ENCUENTRA_EL_NUMERO.value)
--- MiniGames/page/juego1.py
+++ MiniGames/page/juego1.py
@@ -1,5 +1,8 @@
 import reflex as rx
 
+from MiniGames.Components.navbar import navbar
+from MiniGames.styles import Colores
+
 
 class stateTexto(rx.State):
     pass
@@ -14,4 +17,13 @@ def juego() -> rx.Component:
     pass
 
 def pantalla_juego1() -> rx.Component:
-    pass
+    rx.box(
+        rx.vstack(
+        navbar(),
+        align_items="stretch",
+        width="100%"
+        ),
+        bg=Colores.BG.value,
+        background_size="cover",
+        min_height="100vh"
+    )
