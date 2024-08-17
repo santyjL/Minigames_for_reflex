40c46fcdc04a771598e62c6e7723e1dcdcd7cd21
santyjL <jlopezsanty1@gmail.com>
2024-08-17 14:21:23 -0600

    se creo el archivo .py del primer juego

---
+++ MiniGames/page/juego1.py
@@ -0,0 +1,17 @@
+import reflex as rx
+
+
+class stateTexto(rx.State):
+    pass
+
+class stateColor(rx.State):
+    pass
+
+def texto_enunciado() -> rx.Component:
+    pass
+
+def juego() -> rx.Component:
+    pass
+
+def pantalla_juego1() -> rx.Component:
+    pass
