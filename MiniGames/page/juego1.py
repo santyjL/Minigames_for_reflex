2b943fbd93a44a48fe1aa463d4322228d05ccfc4
santyjL <jlopezsanty1@gmail.com>
2024-08-17 15:11:33 -0600

    se creo la clase numeroRandom del juego encuentra el numero

--- MiniGames/page/juego1.py
+++ MiniGames/page/juego1.py
@@ -1,9 +1,45 @@
+import random
+
 import reflex as rx
 
 from MiniGames.Components.navbar import navbar
 from MiniGames.routers import routers
 from MiniGames.styles import Colores
 
+numero_random : int = random.randint(1 , 101)
+
+class numeroRandom(rx.state):
+   numero:int = 0
+
+   @rx.var
+   def sumar10(self):
+       if self.numero <= 90:
+        return self.numero +10
+
+   @rx.var
+   def sumar5(self):
+       if self.numero <=95:
+        return self.numero +5
+
+   @rx.var
+   def sumar1(self):
+       if self.numero <=99:
+        return self.numero +1
+
+   @rx.var
+   def restar10(self):
+       if self.numero >= 10:
+        return self.numero -10
+
+   @rx.var
+   def restar5(self):
+       if self.numero >= 5:
+        return self.numero -5
+
+   @rx.var
+   def restar6(self):
+       if self.numero >= 1:
+        return self.numero -1
 
 class stateTexto(rx.State):
     pass
@@ -12,7 +48,9 @@ class stateColor(rx.State):
     pass
 
 def texto_enunciado() -> rx.Component:
-    pass
+    return rx.box(
+        rx.text("")
+    )
 
 def juego() -> rx.Component:
     pass
