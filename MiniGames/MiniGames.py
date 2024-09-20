4a2da3bd42cea4adde9e1db0448e373ff36cec11
santyjL <jlopezsanty1@gmail.com>
2024-09-19 20:01:59 -0600

    se modifico el rx.accordion al punto que ya tiene un diseño responsivo, un _hover, un bg color transparente y un texto justificado y una que otra cosa de los style que no son importantes.

--- MiniGames/MiniGames.py
+++ MiniGames/MiniGames.py
@@ -2,6 +2,7 @@
 
 import reflex as rx
 
+from MiniGames.page.FQA import FQA_pantalla
 from MiniGames.page.juego1 import pantalla_juego1
 from MiniGames.page.principal import pantalla_principal
 from MiniGames.routers import routers
