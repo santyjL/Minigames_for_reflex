74fef7d87c3912a0858b6c4fed9917d1831d77b7
santyjL <jlopezsanty1@gmail.com>
2025-01-29 11:51:53 -0600

    se actualizó el script de construcción para incluir la URL de la API y se configuraron los orígenes permitidos en rxconfig.py

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ python -m venv .venv
 source .venv/Scripts/activate
 pip install -r requirements.txt
 py -m reflex init
-reflex export
+API_URL=https://minigamesforreflex-production.up.railway.app reflex export
 unzip frontend.zip -d frontend
 unzip backend.zip -d backend
 deactive
--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="MiniGames",
-    backend_host="0.0.0.0",
-    backend_port=8080
+    api_url="minigamesforreflex-production.up.railway.app",  # URL que te dio Railway
+    deploy_url="https://minigames-with-reflex.vercel.app",
+    cors_allowed_origins=["https://minigames-with-reflex.vercel.app"]
 )
