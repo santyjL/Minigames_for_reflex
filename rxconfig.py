f702f8d2027a5f3a016c77b98e51712de9a91342
santyjL <jlopezsanty1@gmail.com>
2025-01-29 18:19:05 -0600

    se eliminó la configuración de orígenes permitidos en rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,4 @@ config = rx.Config(
     app_name="MiniGames",
     api_url="https://minigamesforreflex-production.up.railway.app",  # URL que te dio Railway
     deploy_url="https://minijuegos-reflex.vercel.app/",
-    cors_allowed_origins=["https://minigames-with-reflex.vercel.app"]
 )
