e943aba44da237360f240d1b4bc724947ccc8a0e
santyjL <jlopezsanty1@gmail.com>
2025-01-29 19:01:42 -0600

    se actualizó la URL de despliegue en rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,5 @@ config = rx.Config(
     app_name="MiniGames",
     backend_host="0.0.0.0",
     backend_port=8000,
-    deploy_url="https://minijuegos-backend.railway.app",  # URL pública de Railway
+    deploy_url="https://minigamesforreflex-production.up.railway.app",  # URL pública de Railway
 )
