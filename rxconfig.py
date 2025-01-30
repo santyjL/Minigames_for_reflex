70dca65ce782e2fff813bd459fa91008ef2a4145
santyjL <jlopezsanty1@gmail.com>
2025-01-29 18:17:17 -0600

    se actualizó la URL de despliegue en rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="MiniGames",
-    api_url="minigamesforreflex-production.up.railway.app",  # URL que te dio Railway
-    deploy_url="https://minigames-with-reflex.vercel.app",
+    api_url="https://minigamesforreflex-production.up.railway.app",  # URL que te dio Railway
+    deploy_url="https://minijuegos-reflex.vercel.app/",
     cors_allowed_origins=["https://minigames-with-reflex.vercel.app"]
 )
