91b932e18fa9f87a87270c40b2dc7065e4c6a973
santyjL <jlopezsanty1@gmail.com>
2025-02-04 19:38:20 -0600

    se eliminaron las configuraciones de backend y la URL de despliegue en rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="MiniGames",
-    backend_host="0.0.0.0",
-    backend_port=8000,
-    deploy_url="https://minigamesforreflex-production.up.railway.app",  # URL pública de Railway
+
 )
