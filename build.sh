c09ddea652b87cbf9a79ee909131a119ccbf92eb
santyjL <jlopezsanty1@gmail.com>
2025-02-04 20:11:33 -0600

    se actualizó el script de construcción build.sh para cambiar la gestión de directorios de exportación del frontend

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 source .venv/Scripts/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf public
+rm -rf frontend
 reflex init
 API_URL=https://minigamesforreflex-production.up.railway.app reflex export --frontend-only
-unzip frontend.zip -d public
+unzip frontend.zip -d frontend
 rm -f frontend.zip
 deactivate
